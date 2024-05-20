8e5ab0c5f5b9c078845969a00ecba39e5c24dfbf
Alan <alan_cd@hotmail.com>
2024-05-20 15:54:38 -0300

    test: remove all component tests

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
     e2e: {
         baseUrl: "http://localhost:5173/",
-        specPattern: "../cypress/tests/**/*.spec.{js,jsx,ts,tsx}",
+        specPattern: "cypress/tests/**/*.spec.{js,jsx,ts,tsx}",
         execTimeout: 8 * 1000,
         setupNodeEvents(on, config) {
             // implement node event listeners here
